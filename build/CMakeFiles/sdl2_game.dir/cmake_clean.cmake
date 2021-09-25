75d94f3cf90c1acdceefbb75d620076110af4bcc
cryptigo <connorfrancey31@gmail.com>
2021-09-25 14:11:18 -0600

    first commit

---
+++ build/CMakeFiles/sdl2_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdl2_game.dir/src/main.cpp.o"
+  "sdl2_game"
+  "sdl2_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdl2_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cry/dev/sdl2_game
+# Build directory: /home/cry/dev/sdl2_game/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
